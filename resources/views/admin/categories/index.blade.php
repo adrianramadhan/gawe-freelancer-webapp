17adce9c7a29b5c0387fbc66c00a34f37b304835
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-07-26 13:19:02 +0700

    feat: save new category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,7 +4,7 @@
             <h2 class="font-semibold text-xl text-gray-200 leading-tight">
                 {{ __('Manage Categories') }}
             </h2>
-            <a href="{{route('admin.categories.create')}}" class="font-bold py-4 px-6 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full">
+            <a href="{{route('admin.categories.create')}}" class="font-bold py-4 px-6 bg-indigo-600 hover:bg-indigo-700 text-white rounded-full">
                 Add New
             </a>
         </div>
@@ -17,14 +17,14 @@
                 @forelse ($categories as $category)
                 <div class="item-card flex flex-row justify-between items-center bg-gray-700 p-5 rounded-lg">
                     <div class="flex flex-row items-center gap-x-3">
-                        <img src="#" alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
+                        <img src="{{Storage::url($category->icon)}}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-400 text-xl font-bold">Marketing</h3>
+                            <h3 class="text-gray-200 text-xl font-bold">{{$category->name}}</h3>
                         </div>
                     </div>
                     <div  class="hidden md:flex flex-col text-gray-300">
                         <p class="text-slate-400 text-sm">Date</p>
-                        <h3 class="text-indigo-400 text-xl font-bold">22 Jan 2024</h3>
+                        <h3 class="text-gray-200 text-xl font-bold">{{$category->created_at->format('M d, Y')}}</h3>
                     </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
                         <a href="#" class="font-bold py-4 px-6 bg-indigo-600 hover:bg-indigo-700 text-white rounded-full">
