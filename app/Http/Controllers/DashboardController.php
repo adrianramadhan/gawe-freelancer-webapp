734f7e2e95e1ae030568494a67a3dbad6ff27322
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-07-28 21:27:59 +0700

    feat: wallet transaction feature

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WalletTransaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function wallet()
+    {
+        $user = Auth::user();
+
+        // topup, withdrawal, revenue, expense
+        $wallet_transaction = WalletTransaction::where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return view('dashboard.wallet', compact('wallet_transaction'));
+    }
 }
